1f80404c36c3c6d7345750e8ce0c99aa541304bd
amulshinde76 <136080668+amulshinde76@users.noreply.github.com>
2023-10-27 16:31:44 +0530

    feat: connected to the db (#1)

---
+++ db/connect.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+
+
+const connectDB = async ( url ) => {
+
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    })
+}
+
+module.exports = connectDB
